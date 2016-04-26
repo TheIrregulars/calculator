1af625a26a7af95319f4878b4f5902927a805458
Brian Charlton <brian.s.charlton87@gmail.com>
2016-04-26 13:10:08 -0400

    save file from JSFiddle

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+var number1 = parseInt(prompt("Choose any number:"));
+var number2 = parseInt(prompt("Choose another number:"));
+var result = add(number1,number2);
+alert(result);
