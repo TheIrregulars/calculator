f4253f917b165bc103fb7ddca01f1c88bb7fc260
Brian Charlton <brian.s.charlton87@gmail.com>
2016-04-26 17:21:37 -0400

    finish primitive calc; can't choose between operations

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,7 @@ var subtract = function(number1, number2) {
 var number1 = parseInt(prompt("Choose any number:"));
 var number2 = parseInt(prompt("Choose another number:"));
 var resultS = subtract(number1,number2);
+alert(resultS);
 
 var multiply = function(number1, number2) {
 	return number1 * number2;
@@ -19,6 +20,7 @@ var multiply = function(number1, number2) {
 var number1 = parseInt(prompt("Choose any number:"));
 var number2 = parseInt(prompt("Choose another number:"));
 var resultM = multiply(number1, number2);
+alert(resultM);
 
 var divide = function(number1, number2) {
 	return number1 / number2;
@@ -26,4 +28,5 @@ var divide = function(number1, number2) {
 var number1 = parseInt(prompt("Choose any number:"));
 var number2 = parseInt(prompt("Choose another number:"));
 var resultD = divide(number1, number2);
+alert(resultD);
 
