0142632f37de3b367216b08e2d97663ace7f9b47
Brian Charlton <brian.s.charlton87@gmail.com>
2016-05-01 20:04:59 -0400

    jQuery add event click for numberButton; direct html to jQ

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,15 @@
-var add = function(number1, number2) {
-	return number1 + number2;
-};
-var number1 = parseInt(prompt("Choose any number:"));
-var number2 = parseInt(prompt("Choose another number:"));
-var resultA = add(number1,number2);
-alert(resultA);
+$(function() {
+	$("input.number").click(numberButtonClicked)
+});
 
-var subtract = function(number1, number2) {
-	return number1 - number2;
-};
-var number1 = parseInt(prompt("Choose any number:"));
-var number2 = parseInt(prompt("Choose another number:"));
-var resultS = subtract(number1,number2);
-alert(resultS);
+var currentNumber = "";
 
-var multiply = function(number1, number2) {
-	return number1 * number2;
-};
-var number1 = parseInt(prompt("Choose any number:"));
-var number2 = parseInt(prompt("Choose another number:"));
-var resultM = multiply(number1, number2);
-alert(resultM);
+function numberButtonClicked(clickEvent) {
+	var numberClicked = clickEvent.target.value
 
-var divide = function(number1, number2) {
-	return number1 / number2;
+	currentNumber = currentNumber + numberClicked
+
+	$("span#prompt").html(currentNumber)  
 };
-var number1 = parseInt(prompt("Choose any number:"));
-var number2 = parseInt(prompt("Choose another number:"));
-var resultD = divide(number1, number2);
-alert(resultD);
+
 
