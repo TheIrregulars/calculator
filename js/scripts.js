03203725cbc0454adce3c95c09001e85199dd7ff
Brian Charlton <brian.s.charlton87@gmail.com>
2016-05-02 00:31:41 -0400

    move } to bottom of operator function; needs tweeking

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,15 @@ function numberButtonClicked(clickEvent) {
 	currentNumber = currentNumber + numberClicked
 
 	$("span#prompt").html(currentNumber)  
-};
 
+$(function() {
+	$("input.operator").click(operatorButtonClicked)
+});
 
+	var currentOperator = currentNumber + numberClicked;
+
+function operatorButtonClicked(Event) {
+	var operatorClicked = Event.target.value
+
+	$("span#prompt").html(currentOperator)
+};};
